ce3df2e5b6fb89102bc4fc88e87be1b61ffe5693
Bitummit <ignat.700@mail.ru>
2024-03-15 20:19:57 +0300

    Navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,37 @@
 import "./navbar.scss";
+import SearchIcon from '@mui/icons-material/Search';
+import LanguageIcon from '@mui/icons-material/Language';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
 
 export default function Navbar() {
   return (
     <div className="navbar">
-        Navbar
+        <div className="wrapper">
+          <div className="search">
+            <input type="text" placeholder="Search..."/>
+            <SearchIcon />
+          </div>
+          <div className="items">
+            <div className="item">
+              <LanguageIcon className="icon"/> English
+            </div>
+            <div className="item">
+              <DarkModeIcon className="icon"/>
+            </div>
+            <div className="item">
+              <NotificationsIcon className="icon"/>
+              <div className="counter">1</div>
+            </div>
+            <div className="item">
+              <FormatListBulletedIcon className="icon"/>
+            </div>
+            <div className="item">
+              <img src="https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w1200/2023/10/free-images.jpg" alt="" className="avatar"/>
+            </div>
+          </div>
+        </div>
     </div>
   )
 }
