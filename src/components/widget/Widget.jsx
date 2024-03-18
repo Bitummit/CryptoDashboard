7de389326b0bb0c46ca4dfbc1c4fb9f08c538bb1
Papacita <ignat.700@mail.ru>
2024-03-18 13:26:26 +0300

    add props to widgets

--- src/components/widget/Widget.jsx
+++ src/components/widget/Widget.jsx
@@ -1,21 +1,24 @@
-import './widget.scss'
-import image from '../../assets/eth.png'
-import TrendingUpIcon from '@mui/icons-material/TrendingUp';
+import "./widget.scss";
+import TrendingUpIcon from "@mui/icons-material/TrendingUp";
+import TrendingDownIcon from "@mui/icons-material/TrendingDown";
 
-export default function Widget() {
-    // тут пропсы
-    return (
-        <div className="widget">
-            <img src={image} alt="" />
-            <div className="text">
-                <div className="header">
-                    ETH $23,000
-                </div>
-                <div className="time">
-                    <span className="up"><TrendingUpIcon />45%</span>
-                    last week
-                </div>
-            </div>
+export default function Widget(props) {
+  // тут пропсы
+  return (
+    <div className="widget">
+      <img src={props.image} alt="" />
+      <div className="text">
+        <div className="header">
+          {props.name} ${props.price}
         </div>
-    )
+        <div className="time">
+          <span className={props.isUp ? "up" : "down"}>
+            {props.isUp ? <TrendingUpIcon /> : <TrendingDownIcon />}
+            {props.percent}%
+          </span>
+          last week
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,10 @@ import "./home.scss";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
 import Widget from "../../components/widget/Widget";
+import ethImage from '../../assets/eth.png'
+import btcImage from '../../assets/btc.png'
+import ltcImage from '../../assets/ltc.png'
+import solImage from '../../assets/sol.png'
 
 export default function Home() {
   return (
@@ -12,11 +16,11 @@ export default function Home() {
         <div className="content">
           <div className="pageHeader">Dashboard</div>
           <div className="widgets">
-            <Widget />
-            <Widget />
-            <Widget />
-            <Widget />
-        </div>
+            <Widget name="ETH" price="3,558.56" image={ethImage} percent={0.07} isUp={false}/>
+            <Widget name="BTC" price="72,000.23" image={btcImage} percent={2} isUp={true}/>
+            <Widget name="LTC" price="83.91" image={ltcImage} percent={10} isUp={false}/>
+            <Widget name="SOL" price="210.80" image={solImage} percent={12} isUp={true}/>
+          </div>
         </div>
       </div>
     </div>
