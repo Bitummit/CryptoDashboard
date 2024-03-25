3afeee0a83bf75be9625335ffa69e5c14bf4a0d9
Papacita <ignat.700@mail.ru>
2024-03-25 12:18:31 +0300

    datatable

--- src/components/datatable/Datatable.jsx
+++ src/components/datatable/Datatable.jsx
@@ -1,109 +1,48 @@
 import "./datatable.scss";
 import { DataGrid } from "@mui/x-data-grid";
+import { rows } from "./data";
 
 const columns = [
   { field: "id", headerName: "ID", width: 70 },
   { field: "firstName", headerName: "First name", width: 200 },
   { field: "lastName", headerName: "Last name", width: 200 },
-  // {
-  //   field: 'age',
-  //   headerName: 'Age',
-  //   type: 'number',
-  //   width: 90,
-  // },
   { field: "login", headerName: "Login", width: 200 },
   { field: "email", headerName: "Email", width: 200 },
-  // {
-  //   field: 'fullName',
-  //   headerName: 'Full name',
-  //   description: 'This column has a value getter and is not sortable.',
-  //   sortable: false,
-  //   width: 160,
-  //   valueGetter: (value, row) => `${row.firstName || ''} ${row.lastName || ''}`,
-  // },
-];
-
-const rows = [
+  { field: "test", headerName: "TestField", width: 200 },
+  { field: "status", headerName: "Status", width: 200 },
   {
-    id: 1,
-    lastName: "Snow",
-    firstName: "Jon",
-    login: "Papacita",
-    email: "1@mail.ru",
-  },
-  {
-    id: 2,
-    lastName: "Lannister",
-    firstName: "Cersei",
-    login: "Psina",
-    email: "2@mail.ru",
-  },
-  {
-    id: 3,
-    lastName: "Lannister",
-    firstName: "Jaime",
-    login: "Bitum",
-    email: "3@mail.ru",
-  },
-  {
-    id: 4,
-    lastName: "Stark",
-    firstName: "Arya",
-    login: "Kolyan",
-    email: "4@mail.ru",
-  },
-  {
-    id: 5,
-    lastName: "Targaryen",
-    firstName: "Daenerys",
-    login: "Mom",
-    email: "5@mail.ru",
-  },
-  {
-    id: 6,
-    lastName: "Melisandre",
-    firstName: null,
-    login: "Shtanga",
-    email: "6@mail.ru",
-  },
-  {
-    id: 7,
-    lastName: "Clifford",
-    firstName: "Ferrara",
-    login: "Qwerty",
-    email: "7@mail.ru",
-  },
-  {
-    id: 8,
-    lastName: "Frances",
-    firstName: "Rossini",
-    login: "Jeep",
-    email: "8@mail.ru",
-  },
-  {
-    id: 9,
-    lastName: "Roxie",
-    firstName: "Harvey",
-    login: "Pes",
-    email: "9@mail.ru",
+    field: "action",
+    headerName: "Action",
+    width: 220,
+    renderCell: () => {
+      return (
+        <div className="cellAction">
+          <a className="viewButton" href="#">View</a>
+          <a className="deleteButton" href="#">Delete</a>
+        </div>
+      );
+    },
   },
 ];
 
 export default function DataTable() {
   return (
-    <div className="datatable">
-      <DataGrid
-        className="data"
-        rows={rows}
-        columns={columns}
-        initialState={{
-          pagination: {
-            paginationModel: { page: 0, pageSize: 15 },
-          },
-        }}
-        pageSizeOptions={[5, 10]}
-        checkboxSelection
-      />
+    <div className="content">
+      <div className="title">Users data</div>
+      <button className="addButton">+ Add user</button>
+      <div className="datatable">
+        <DataGrid
+          className="data"
+          rows={rows}
+          columns={columns}
+          initialState={{
+            pagination: {
+              paginationModel: { page: 0, pageSize: 15 },
+            },
+          }}
+          checkboxSelection
+        />
+      </div>
     </div>
   );
 }
