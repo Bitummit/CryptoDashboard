92d97f61962d402e59d048301636c05329ca26f3
Papacita <ignat.700@mail.ru>
2024-04-01 09:28:48 +0300

    pie chart theme color

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,12 +1,14 @@
 import "./featured.scss";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import ReactApexChart from "react-apexcharts";
+import { useContext } from "react";
+import { DarkModeContext } from "../../context/darkModeContext";
 
 const data = {
   series: [1023.2, 23042.12, 3821.61, 12412.01],
   options: {
     chart: {
-      type: 'donut'
+      type: "donut",
     },
     plotOptions: {
       pie: {
@@ -16,28 +18,26 @@ const data = {
             total: {
               showAlways: true,
               show: true,
-              label: 'Total',
-              fontSize: '20px',
-              fontWeight: 'bold',
-            }
+              label: "Total",
+              fontSize: "20px",
+              fontWeight: "bold",
+            },
           },
-          
-        }
-      }
+        },
+      },
     },
     legend: {
-      fontSize: '16px',
-      position: 'bottom',
+      fontSize: "16px",
+      position: "bottom",
       labels: {
         // colors: 'white',
-        // colors: darkMode ? 'white' : '#222',
-        useSeriesColors: false
+        // colors: theme.darkMode ? 'white' : '#222',
+        useSeriesColors: false,
       },
     },
     labels: ["ETH", "BTC", "LTC", "SOL"],
-    colors: ['#658ed7', '#f5c627', '#5e685c', '#daa9d2'],
-  }
-
+    colors: ["#658ed7", "#f5c627", "#5e685c", "#daa9d2"],
+  },
 };
 
 let total = 0;
@@ -47,6 +47,10 @@ data.series.forEach((num) => {
 });
 
 export default function Featured() {
+  
+  const theme = useContext(DarkModeContext);
+  data.options.legend.labels.colors = theme.darkMode ? "rgb(184, 179, 179);" : "#222";
+
   return (
     <div className="featured">
       <div className="top">
