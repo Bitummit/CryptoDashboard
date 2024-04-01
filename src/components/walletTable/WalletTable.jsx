26f61547016f67886b24375efe592327808a9ea5
Bitummit <ignat.700@mail.ru>
2024-04-01 22:44:12 +0300

    wallet table

--- src/components/walletTable/WalletTable.jsx
+++ src/components/walletTable/WalletTable.jsx
@@ -1,5 +1,7 @@
 /* eslint-disable react/prop-types */
 import "./walletTable.scss";
+import { DataGrid } from "@mui/x-data-grid";
+
 
 export default function WalletTable(props) {
   let total = 0;
@@ -9,7 +11,26 @@ export default function WalletTable(props) {
   props.data.map((coin) => {
     coin.portfolio = parseFloat((coin.balance / total) * 100).toFixed(2);
   });
-  console.log(props.data);
+  const columns =[
+    { field: "name", headerName: "Token", flex: 1,headerAlign: 'center', cellClassName: 'name',},
+    { 
+      field: "amount",
+      headerName: "Amount", 
+      flex: 1, 
+      headerAlign: 'center',
+      renderCell: (params) => {
+        return (
+          <div className="amount">
+            {params.row.amount}<img className="icon" src={params.row.image} /> 
+          </div>
+        );
+      },
+    },
+    { field: "portfolio", headerName: "Portfolio %", flex: 1, headerAlign: 'center', cellClassName: 'portfolio',},
+    { field: "price", headerName: "Price(24h)", flex: 1, headerAlign: 'center', cellClassName: 'price', },
+    { field: "balance", headerName: "Balance", flex: 1, headerAlign: 'center', cellClassName: 'balance', }
+  ]
+
   return (
     <div className="walletTable">
       <div className="totalStatistic">
@@ -17,25 +38,20 @@ export default function WalletTable(props) {
         <h1>$ {total.toLocaleString()}</h1>
         <p className="up">+$1201.47 (+3.8%)</p>
       </div>
-
-      <table className="table">
-        <tr>
-          <th>Token</th>
-          <th>Amount</th>
-          <th>Portfolio %</th>
-          <th>Price(24h)</th>
-          <th>Balance</th>
-        </tr>
-        {props.data.map((coin) => (
-          <tr key={coin.id}>
-            <td>{coin.name}</td>
-            <td>{coin.amount}</td>
-            <td>{coin.portfolio}%</td>
-            <td>{coin.price}</td>
-            <td>{coin.balance}</td>
-          </tr>
-        ))}
-      </table>
+      <div className="datatable">
+        <DataGrid
+          className="data"
+          rows={props.data}
+          columns={columns}
+          disableColumnFilter
+          disableColumnMenu
+          disableColumnResize
+          disableRowSelectionOnClick
+          hideFooter
+          rowHeight={70}
+        />
+      </div>
+      
     </div>
   );
 }
--- src/pages/wallet/Wallet.jsx
+++ src/pages/wallet/Wallet.jsx
@@ -9,6 +9,7 @@ export default function Wallet() {
         {
             id: 1,
             name: 'ETH',
+            image: "/src/assets/eth.png",
             amount: 1.2301,
             price: 3558.56,
             balance: 4336.14,
@@ -16,6 +17,7 @@ export default function Wallet() {
         {
             id: 2,
             name: 'BTC',
+            image: "/src/assets/btc.png",
             amount: 0.12821,
             price: 72000,
             balance: 9231.56,
@@ -23,6 +25,7 @@ export default function Wallet() {
         {
             id: 3,
             name: 'LTC',
+            image: "/src/assets/ltc.png",
             amount: 154.65756,
             price: 83.91,
             balance: 12977.07,
@@ -30,6 +33,7 @@ export default function Wallet() {
         {
             id: 4,
             name: 'SOL',
+            image: "/src/assets/sol.png",
             amount: 21.435612,
             price: 210.80,
             balance: 4518.80,
