21f6d5f6a850fb3928c5200e838bf3c7f9c58388
Papacita <ignat.700@mail.ru>
2024-04-02 10:15:37 +0300

    wallet table ui

--- src/components/walletTable/WalletTable.jsx
+++ src/components/walletTable/WalletTable.jsx
@@ -3,40 +3,73 @@ import "./walletTable.scss";
 import { DataGrid } from "@mui/x-data-grid";
 import { useEffect, useState } from "react";
 
-
 export default function WalletTable(props) {
-
-  const [total, setTotal] = useState(0)
+  const [total, setTotal] = useState(0);
 
   useEffect(() => {
-    setTotal(0)
+    setTotal(0);
     props.data.forEach((item) => {
-      setTotal(prev => prev + item.balance)
+      setTotal((prev) => prev + item.balance);
     });
     props.data.map((coin) => {
       coin.portfolio = parseFloat((coin.balance / total) * 100).toFixed(2);
     });
-  }, []);
-  
-  const columns =[
-    { field: "name", headerName: "Token", flex: 1,headerAlign: 'center', cellClassName: 'name',},
-    { 
-      field: "amount",
-      headerName: "Amount", 
-      flex: 1, 
-      headerAlign: 'center',
+  }, [total]);
+
+  const columns = [
+    {
+      field: "name",
+      headerName: "Token",
+      flex: 1,
+      headerAlign: "center",
       renderCell: (params) => {
         return (
-          <div className="amount">
-            {params.row.amount}<img className="icon" src={params.row.image} /> 
+          <div className="name">
+            
+            <img className="nameIcon" src={params.row.image} /> {params.row.name}
           </div>
         );
       },
     },
-    { field: "portfolio", headerName: "Portfolio %", flex: 1, headerAlign: 'center', cellClassName: 'portfolio',},
-    { field: "price", headerName: "Price(24h)", flex: 1, headerAlign: 'center', cellClassName: 'price', },
-    { field: "balance", headerName: "Balance", flex: 1, headerAlign: 'center', cellClassName: 'balance', }
-  ]
+    {
+      field: "amount",
+      headerName: "Amount",
+      flex: 1,
+      headerAlign: "center",
+      renderCell: (params) => {
+        return (
+          <div className="amount">
+            {params.row.amount}
+            <img className="icon" src={params.row.image} />
+          </div>
+        );
+      },
+    },
+    {
+      field: "portfolio",
+      headerName: "Portfolio %",
+      flex: 1,
+      headerAlign: "center",
+      cellClassName: "portfolio",
+      valueGetter: (value) => value +"%",
+    },
+    {
+      field: "price",
+      headerName: "Price(24h)",
+      flex: 1,
+      headerAlign: "center",
+      cellClassName: "price",
+      valueGetter: (value) => "$" + value,
+    },
+    {
+      field: "balance",
+      headerName: "Balance",
+      flex: 1,
+      headerAlign: "center",
+      cellClassName: "balance",
+      valueGetter: (value) => "$" + value,
+    },
+  ];
   return (
     <div className="walletTable">
       <div className="totalStatistic">
@@ -57,7 +90,6 @@ export default function WalletTable(props) {
           rowHeight={70}
         />
       </div>
-      
     </div>
   );
 }
