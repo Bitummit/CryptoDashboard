7ba2aa31fd14f347548a00bbe7c4b66b0efe6ecb
Papacita <ignat.700@mail.ru>
2024-04-10 09:17:22 +0300

    test swap form

--- src/components/swap/Swap.jsx
+++ src/components/swap/Swap.jsx
@@ -1,45 +1,55 @@
 import "./swap.scss";
-import { useState, useEffect } from "react";
-import InputLabel from "@mui/material/InputLabel";
+import { useState } from "react";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
+import KeyboardDoubleArrowDownIcon from "@mui/icons-material/KeyboardDoubleArrowDown";
 
 export default function Swap(props) {
   const initValue = props.data[Math.floor(Math.random() * props.data.length)];
-  const [token, setToken] = useState(initValue);
+  const [tokenFrom, setTokenFrom] = useState(initValue);
 
   const handleChange = (event) => {
     const name = event.target.value;
     const item = props.data.find((item) => {
       return item.name === name;
     });
-    setToken(item);
+    setTokenFrom(item);
   };
 
   return (
     <div className="swap">
       <div className="converter">
-        <div className="menu">
+        <div className="from">
           <FormControl sx={{ m: 1, minWidth: 80, height: 2 }}>
             <Select
-              value={token.name}
+              value={tokenFrom.name}
               onChange={handleChange}
               autoWidth
               variant={"standard"}
             >
               {props.data.map((item) => {
                 return (
-                  <MenuItem key={item.id} value={item.name} className="dropItems">
-                    <img src={item.image}/>{item.name}
+                  <MenuItem
+                    key={item.id}
+                    value={item.name}
+                    className="dropItem"
+                  >
+                    <img src={item.image} />
+                    {item.name}
                   </MenuItem>
                 );
               })}
             </Select>
           </FormControl>
+          <input placeholder={tokenFrom.amount} />
         </div>
-        <input placeholder={token.amount} />
+        <div className="arrow">
+          <KeyboardDoubleArrowDownIcon className="icon" />
+        </div>
+        <div className="to"></div>
       </div>
+
       <div className="table">table</div>
     </div>
   );
