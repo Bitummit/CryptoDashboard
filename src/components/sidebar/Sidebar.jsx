fa6d2060e54f0a7c4c7b7ff05e1e731ac369e888
Papacita <ignat.700@mail.ru>
2024-04-26 16:29:20 +0300

    nimation for sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -10,24 +10,60 @@ import { useContext, useState } from "react";
 import { DarkModeContext } from "../../context/darkModeContext";
 import DehazeIcon from "@mui/icons-material/Dehaze";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import btcImage from "../../assets/btc.png";
 
 export default function Sidebar() {
   const { dispatch } = useContext(DarkModeContext);
   const [collapsed, setCollapsed] = useState(false);
 
+  function showText() {
+    let elements = [...document.getElementsByClassName("hiddenText")];
+
+    elements.forEach((element) => {
+      //   if(element.classList.includes("logo")){
+      //     element.style.display = "flex";
+      //   } else{
+      //     element.style.display = "flex";
+      // }
+      element.style.display = "flex";
+      setTimeout(() => {
+        element.style.opacity = 1;
+      }, 100);
+    });
+  }
+
   return (
     <div className={`sidebar ${collapsed ? "collapsed" : ""}`}>
       <div className="top">
-        <span className="logo"></span>
+        <span className="logo hiddenText">
+          <img src={btcImage} />
+          Panel
+        </span>
         <button
           onClick={() => {
             setCollapsed(!collapsed);
+
+            let sidebar = document.getElementsByClassName("sidebar")[0];
+
+            let elements = [...document.getElementsByClassName("hiddenText")];
+            console.log(elements);
+            if (collapsed) {
+              sidebar.style.flex = 0.3;
+
+              elements.forEach((element) => {
+                element.style.display = "none";
+                element.style.opacity = 0;
+              });
+            } else {
+              sidebar.style.flex = 1.1;
+              setTimeout(showText, 400);
+            }
           }}
         >
           {collapsed ? (
-            <ArrowBackIcon className="icon" />
+            <ArrowBackIcon className="icon back" />
           ) : (
-            <DehazeIcon className="icon " />
+            <DehazeIcon className="icon open" />
           )}
         </button>
       </div>
@@ -38,7 +74,7 @@ export default function Sidebar() {
             <li>
               <div className="iconWarpper">
                 <DashboardIcon className="icon" />
-                <span>{collapsed ? "Dashboard" : ""}</span>
+                <span className="hiddenText">Dashboard</span>
               </div>
             </li>
           </Link>
@@ -46,7 +82,7 @@ export default function Sidebar() {
             <li>
               <div className="iconWarpper">
                 <GroupIcon className="icon" />
-                <span>{collapsed ? "Users" : ""}</span>
+                <span className="hiddenText">Users</span>
               </div>
             </li>
           </Link>
@@ -54,26 +90,26 @@ export default function Sidebar() {
             <li>
               <div className="iconWarpper">
                 <AccountBalanceWalletIcon className="icon" />
-                <span>{collapsed ? "Wallet" : ""}</span>
+                <span className="hiddenText">Wallet</span>
               </div>
             </li>
           </Link>
           <li>
             <div className="iconWarpper">
               <SettingsIcon className="icon" />
-              <span>{collapsed ? "Settings" : ""}</span>
+              <span className="hiddenText">Settings</span>
             </div>
           </li>
           <li>
             <div className="iconWarpper">
               <AccountCircleIcon className="icon" />
-              <span>{collapsed ? "Profile" : ""}</span>
+              <span className="hiddenText">Profile</span>
             </div>
           </li>
           <li>
             <div className="iconWarpper">
               <LogoutIcon className="icon" />
-              <span>{collapsed ? "Logout" : ""}</span>
+              <span className="hiddenText">Logout</span>
             </div>
           </li>
         </ul>
